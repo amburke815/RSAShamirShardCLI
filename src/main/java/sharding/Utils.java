bf66fbcf79f3be4496b080936a20f60b509337ca
amburke815 <burke.ad@northeastern.edu>
2022-03-07 22:37:49 -0500

    completed initial implementation of RSA encryption and decryption methods

---
+++ src/main/java/sharding/Utils.java
@@ -0,0 +1,17 @@
+package sharding;
+
+public class Utils {
+
+  public static <X> X notNull(X toCheck) {
+    if (toCheck == null)
+      throw new IllegalArgumentException("passed a null argument to a non-null parameter");
+    return toCheck;
+  }
+
+  public static int intBetween(int lowerBoundIncl, int toCheck, int upperBoundIncl) {
+    if (toCheck < lowerBoundIncl || toCheck > upperBoundIncl)
+      throw new IllegalArgumentException("int provided not within inclusive bounds ["
+          + lowerBoundIncl + "," + upperBoundIncl + "]");
+    return toCheck;
+  }
+}
--- src/main/java/sharding/cli/ShardingCLIImpl.java
+++ src/main/java/sharding/cli/ShardingCLIImpl.java
@@ -1,14 +1,23 @@
 package sharding.cli;
 
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.List;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import sharding.Utils;
 import sharding.shards.IKeyShard;
 
 public class ShardingCLIImpl implements IShardingCLI {
 
   private static int defaultKeySize = 2048;
+  private static String keyType = "RSA";
 
   @Override
   public String helpMenu() {
@@ -23,8 +32,8 @@ public class ShardingCLIImpl implements IShardingCLI {
   @Override
   public KeyPair RSAKeyGen(int keySize) {
     try {
-      KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-      kpg.initialize(keySize);
+      KeyPairGenerator kpg = KeyPairGenerator.getInstance(keyType);
+      kpg.initialize(Utils.intBetween(1, keySize, Integer.MAX_VALUE));
       return kpg.generateKeyPair();
 
     } catch (NoSuchAlgorithmException e) {
@@ -44,22 +53,40 @@ public class ShardingCLIImpl implements IShardingCLI {
 
   @Override
   public String encrypt(String plainText, KeyPair encryptWith) {
-    return null;
+    try {
+      Cipher encCipher = Cipher.getInstance(keyType);
+      encCipher.init(Cipher.ENCRYPT_MODE, encryptWith.getPublic());
+      byte[] plainTextBytes = plainText.getBytes(StandardCharsets.UTF_8);
+      byte[] cipherTextBytes = encCipher.doFinal(plainTextBytes);
+      return Base64.getEncoder().encodeToString(cipherTextBytes);
+    } catch (NoSuchAlgorithmException | InvalidKeyException | IllegalBlockSizeException |
+        BadPaddingException | NoSuchPaddingException e) {
+      throw new IllegalStateException("caught an " + e.getClass() + " when encrypting ciphertext");
+    }
   }
 
   @Override
   public String encrypt(String plainText, List<IKeyShard> shards) {
-    return null;
+    return encrypt(plainText, assembleShards(shards));
   }
 
   @Override
   public String decrypt(String cipherText, KeyPair decryptWith) {
-    return null;
+    try {
+      Cipher decCipher = Cipher.getInstance(keyType);
+      decCipher.init(Cipher.DECRYPT_MODE, decryptWith.getPublic());
+      byte[] cipherTextBytes = cipherText.getBytes(StandardCharsets.UTF_8);
+      byte[] plainTextBytes = decCipher.doFinal(cipherTextBytes);
+      return new String(plainTextBytes, StandardCharsets.UTF_8);
+    } catch (NoSuchAlgorithmException | InvalidKeyException | IllegalBlockSizeException |
+        BadPaddingException | NoSuchPaddingException e) {
+      throw new IllegalStateException("caught an " + e.getClass() + " when encrypting ciphertext");
+    }
   }
 
   @Override
   public String decrypt(String cipherText, List<IKeyShard> shards) {
-    return null;
+    return decrypt(cipherText, assembleShards(shards));
   }
 
   @Override
