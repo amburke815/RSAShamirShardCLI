f7f1d040787a13ecac25ad6a18434668f79d75ae
amburke815 <burke.ad@northeastern.edu>
2022-03-15 00:29:38 -0500

    main unit test passing. Needs some type redesign, docs, and README

--- src/test/java/tests/KeyShardingTests.java
+++ src/test/java/tests/KeyShardingTests.java
@@ -1,7 +1,14 @@
 package tests;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
 import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPrivateCrtKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Map.Entry;
+import java.util.Random;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,6 +22,40 @@ public class KeyShardingTests {
   public String randomStringPlainText = RandomStringUtils.random(randomStringLength, true, true);
   private IShardingCLI shardingCLI = new ShardingCLIImpl();
   private KeyPair kp = shardingCLI.RSAKeyGen();
+
+
+  @Test
+  public void programWorksCorrectly() {
+    IShardingCLI cliInstance = new ShardingCLIImpl();
+    // 1. creates the RSA key pair with a private key broken into 5 shards
+    KeyPair generatedRSAKeyPair = cliInstance.RSAKeyGen();
+    cliInstance.shamirShardKey(generatedRSAKeyPair, 5, 2);
+
+    // 2. encrypts a random plain text string using the RSA key
+    String randomPlainText = RandomStringUtils.random(200, true, true);
+    byte[] encryptedBytes = cliInstance.encrypt(randomPlainText, generatedRSAKeyPair.getPublic());
+
+    // 3. reassembles the private key using shards 2 and 5
+    byte[] reassembledPrivateKeyBytes = cliInstance.assembleShards(2, 5);
+    // TODO: make the above a private key (possibly using KeyFactory)
+    try {
+      KeyFactory kf = KeyFactory.getInstance("RSA");
+      EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(reassembledPrivateKeyBytes);
+      PrivateKey privateKey = kf.generatePrivate(privKeySpec);
+      // 4. decrypts the cipher text back into the plain text using the reassembled Private Key
+      String decrypted = cliInstance.decrypt(encryptedBytes, privateKey);
+      // 5. asserts the decrypted plain text is equal to the original plain text in step 2
+      Assert.assertEquals(randomPlainText, decrypted);
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      Assert.fail();
+    }
+
+
+
+
+  }
 
   @Test
   public void produceRandomString() {
